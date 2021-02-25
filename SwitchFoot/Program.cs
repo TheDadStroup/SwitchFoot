983265ec03c083d38b02a0fc0f0dbd077af7886b
The Dad <Shihen1611@gmail.com>
2021-02-24 19:32:47 -0600

    first push

---
+++ SwitchFoot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchFoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "algebra":
+                    Console.WriteLine("You must be right minded");
+                    break;
+
+                case "English":
+                    Console.WriteLine("You must be left minded");
+                    break;
+
+                case "calculus":
+                    Console.WriteLine("You must be out of your mind");
+
+                    break;
+                case "golf":
+                    Console.WriteLine("You must be a great student");
+                    break;
+                default:
+                    Console.WriteLine($"Really? You need to get out more often");
+                    break;
+
+            }
+            
+
+
+
+
+            
+        }
+    }
+}
